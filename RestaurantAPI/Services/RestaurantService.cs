f591154a280cbcdadc28dd649bc9e50585d5c5b5
Marcin Pałys <marcin.palys@microsoft.wsei.edu.pl>
2025-04-22 21:16:51 +0200

    Filtering results

--- RestaurantAPI/Services/RestaurantService.cs
+++ RestaurantAPI/Services/RestaurantService.cs
@@ -13,7 +13,7 @@ namespace RestaurantAPI.Services
     public interface IRestaurantService
     {
         RestaurantDto GetById(int Id);
-        IEnumerable<RestaurantDto> GetAll();
+        IEnumerable<RestaurantDto> GetAll(string searchPhrase);
         int Create(CreateRestaurantDto dto);
         void Delete(int id);
         void Update(int id, ModificationRestaurantDto dto);
@@ -84,12 +84,13 @@ namespace RestaurantAPI.Services
             var result = _mapper.Map<RestaurantDto>(restaurant);
             return result;
         }
-        public IEnumerable<RestaurantDto> GetAll()
+        public IEnumerable<RestaurantDto> GetAll(string searchPhrase)
         {
             var restaurants = _restaurantDbContext
                 .Restaurants
                 .Include(r => r.Address)
                 .Include(r => r.Dishes)
+                .Where(r => searchPhrase == null ||( r.Name.ToLower().Contains(searchPhrase.ToLower()) || r.Description.ToLower().Contains(searchPhrase.ToLower())))
                 .ToList();
             var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
 
